32d53c226a95ed29de55c67d03623881e5d1031c
vrbdas <vrbdas@gmail.com>
2024-05-24 11:49:37 +0500

    second

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,76 @@
 <script setup>
-import lesson4 from './components/lesson4.vue'
+import UserComp from './components/UserComp.vue';
 </script>
 
 <template>
-  <lesson4 />
+  <div class="form">
+  <input type="text" placeholder="Имя" v-model="userName">
+  <input type="password" placeholder="Пароль" v-model="userPassw">
+  <input type="email" placeholder="Email" v-model="userEmail">
+  <button @click.prevent="send">Отправить</button>
+  <p class="error">{{ error }}</p>
+</div>
+<div v-if="users.length == 0">
+  <h3>Нет пользователей</h3>
+</div>
+  <UserComp v-for="(user, index) in users" :key="index" :user="user" :index="index" />
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+      userName: '',
+      userPassw: '',
+      userEmail: '',
+      error: '',
+    }
+  },
+  
+  methods: {
+    send() {
+      if (this.userName.length < 2) {
+        this.error = 'Имя слишком короткое';
+        return;
+     }
+     if (this.userPassw.length < 3) {
+      this.error = 'Пароль слишком короткий';
+      return;
+     } 
+     if (this.userEmail.length === 0) {
+      this.error = 'Некорректный адрес Email';
+      return;
+     }
+    this.users.push({
+      name: this.userName,
+      passw: this.userPassw,
+      email: this.userEmail,
+    });
+    this.userName = '';
+    this.userPassw = '';
+    this.userEmail = '';
+    this.error = '';
+    }
+  },
+}
+</script>
 
+<style scoped>
+  .form {
+    width: 250px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  .hide {
+    display: none;
+  }
+  button {
+    width: 50%;
+  }
+  .error {
+    color: red;
+  }
 </style>
---
+++ src/components/UserComp.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable no-unused-vars -->
+<script setup>
+const props = defineProps(['user', 'index']);
+</script>
+
+<template>
+  <div class="user">
+    <h3>Пользователь {{ index }}:</h3>
+    Имя: {{ user.name }}<br>
+    Пароль: {{ user.passw }}<br>
+    Email: {{ user.email }}<br>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['user', 'index'],
+}
+</script>
+
+<style scoped>
+  .user {
+    padding: 10px;
+    background-color:aqua;
+    width: 250px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+</style>
